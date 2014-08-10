84d1a43e643d9fea39168ecd7243e4ecfb4beb21
Fernando Diaz <fdiazgarrido@gmail.com>
2014-08-10 19:36:35 +0100

    refactoring class resolution to the Keyword class

--- lib/json/schema/keyword.rb
+++ lib/json/schema/keyword.rb
@@ -10,6 +10,15 @@ module JSON
         raise NotImplementedError.new('You must implement validate.')
       end
 
+      def self.classify(keyword)
+        class_name = keyword + 'Keyword'
+        class_name.sub!(/./) { $&.upcase }
+
+        if JSON::Schema.const_defined?(class_name)
+          JSON::Schema.const_get(class_name) 
+        end
+      end
+
       private
 
       attr_reader :schema, :value
--- lib/json/schema/schema.rb
+++ lib/json/schema/schema.rb
@@ -17,13 +17,9 @@ module JSON
     private
 
     def extract_keywords(schema)
-      schema.map { |k, v| classify(k).new(self, v) if classify(k) }.compact
-    end
-
-    def classify(keyword)
-      class_name = keyword + 'Keyword'
-      class_name.sub!(/./) { $&.upcase }
-      self.class.const_get(class_name) if self.class.const_defined?(class_name)
+      schema.map do |k, v| 
+        Keyword.classify(k).new(self, v) if Keyword.classify(k)
+      end.compact
     end
 
     attr_reader :title
